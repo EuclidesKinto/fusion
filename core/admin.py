bef7f781341dd83570b564a97f7cc8cb5f2ed040
Euclides <euclideskinto@gmail.com>
2021-10-07 20:59:19 -0300

    add models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Cargo, Service, Funcionario
 
-# Register your models here.
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'status', 'updated')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service', 'icon', 'status', 'updated')
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'status', 'updated')

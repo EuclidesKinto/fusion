63b701fcf6c10610b431f691eeaaabff4f8f3ab5
Euclides <euclideskinto@gmail.com>
2021-10-07 21:18:34 -0300

    add dados no template

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
+from .models import Service, Funcionario
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Service.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.all()
+        return context
+
+
